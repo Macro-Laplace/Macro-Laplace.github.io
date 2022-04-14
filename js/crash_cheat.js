039e8c2f1757adc7912dc559be32bdbddd338400
Macro-Laplace <810282264@qq.com>
2022-04-14 22:35:56 +0800

    Site updated: 2022-04-14 22:35:56

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -3,12 +3,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/joke.ico");
-        document.title = '看不见我🙈~看不见我🙈~';
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = ' ( ๑•̀ㅂ•́) ✧被发现了～';
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~';
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
